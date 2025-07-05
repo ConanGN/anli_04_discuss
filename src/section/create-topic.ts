e80b2748dfa4fe4f5c83341c07ad443e7fda83cd
ConanGN <775482085@qq.com>
2025-07-05 12:31:30 +0800

    3.16 表单校验--zod

--- src/section/create-topic.ts
+++ src/section/create-topic.ts
@@ -1,9 +1,23 @@
 "use server"
+import {z} from "zod"
 
+// 定义校验规则
+const createTopSchema = z.object({
+    name: z.string().min(3).regex(/^[a-zA-Z0-9]+$/, {message: "aaaa"}),   
+    //长度最小3位  regex定义只能输入字母和数字  message定义错误提示
+    description: z.string().min(10).max(100)
+})
 
 export default async function createTopic(formData: FormData){
     const name = formData.get("name")
     const description = formData.get("description")
-    console.log(name, description)
+
+    const result = createTopSchema.safeParse({name, description})   //校验
+
+    if(!result.success){
+        console.log(result.error.flatten().fieldErrors)   // flatten() 将错误信息扁平化
+    }
+
+    // console.log(result.data)  
 
 }
